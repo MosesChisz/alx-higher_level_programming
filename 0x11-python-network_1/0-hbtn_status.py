9227293eb9b6868a26682cefb94ca9c0372bb9a0
MosesChisz <mosesc@cortexware.co.za>
2023-12-01 20:14:25 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""This script fetches data from
-    https://alx-intranet.hbtn.io/status
-"""
-
-
+"""  fetches https://intranet.hbtn.io/status  """
 import urllib.request
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        r = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(r)))
+        print("\t- content: {}".format(r))
+        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
